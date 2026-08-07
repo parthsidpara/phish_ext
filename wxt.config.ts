6ef65128c17a4580d96da584348ee7eb4a39c4bc
Parth <parthsidpara2005@gmail.com>
2026-08-07 17:19:56 +0530

    add shortcut to run the demo

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
     name: 'phish_ext',
     host_permissions: ['*://*/*'],
     permissions: ['storage', 'tabs', 'offscreen', 'webNavigation'],
+    commands: {
+      'run-highlight-demo': {
+        suggested_key: { default: 'Ctrl+Shift+H' }, // temporary shortcut for demo purpose
+        description: 'Run the Driver.js highlight demo on the current page',
+      },
+    },
   },
 });
